5faa892edb18b26a8f5421c3823049d28dbd06ea
Deleña, Aldrin M. <152266220+Gawrin@users.noreply.github.com>
2025-01-23 11:16:14 +0000

    Finished the Activity: Simple Routing to Views

--- laravelTesting/routes/web.php
+++ laravelTesting/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\GreetController;
+
+
+Route::get('/hello', function () {
+    return 'Hello, Laravel!';
+});
+
+Route::get('/greet', [GreetController::class, 'showGreeting']);
